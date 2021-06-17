a16fe639bd42966e24b580ed1f456b738391041b
mustangxu <mustangxu@gmail.com>
2021-06-17 19:23:20 +0800

    add datadog integration

--- java-playground/src/main/java/com/jayxu/playground/BaseConfig.java
+++ java-playground/src/main/java/com/jayxu/playground/BaseConfig.java
@@ -5,15 +5,11 @@ package com.jayxu.playground;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author xujiajing
  */
 @SpringBootConfiguration
-@EnableWebMvc
-@EnableJpaRepositories
 @ComponentScan(basePackages = { "com.jayxu" })
 public class BaseConfig {
 }
--- java-playground/src/main/java/com/jayxu/playground/spring/PlaygroundApplication.java
+++ java-playground/src/main/java/com/jayxu/playground/spring/PlaygroundApplication.java
@@ -5,11 +5,15 @@ package com.jayxu.playground.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author xujiajing
  */
 @SpringBootApplication
+@EnableWebMvc
+@EnableJpaRepositories
 public class PlaygroundApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundApplication.class, args);
