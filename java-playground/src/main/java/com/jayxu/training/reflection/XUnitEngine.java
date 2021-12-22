aa4f3f588ab976a87b5118d0436b1ebbe59f9ba5
mustangxu <mustangxu@gmail.com>
2021-12-22 14:19:47 +0800

    #refine code#

--- java-playground/src/main/java/com/jayxu/training/reflection/XUnitEngine.java
+++ java-playground/src/main/java/com/jayxu/training/reflection/XUnitEngine.java
@@ -9,39 +9,40 @@ import org.apache.commons.io.IOUtils;
 
 public class XUnitEngine {
 
-	public static void main(String[] args) throws Exception {
-		Class<?> c = new XUnitClassLoader()
-				.loadClass("com/jayxu/training/reflection/MathTest.class");
-		var cls = c.getConstructor().newInstance();
+    public static void main(String[] args) throws Exception {
+        Class<?> c = new XUnitClassLoader()
+            .loadClass("com/jayxu/training/reflection/MathTest.class");
+        var cls = c.getConstructor().newInstance();
 
-		Arrays.stream(c.getMethods())
-				.filter(m -> m.isAnnotationPresent(Test.class)).forEach(m -> {
-					try {
-						m.invoke(cls);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				});
-	}
+        Arrays.stream(c.getMethods())
+            .filter(m -> m.isAnnotationPresent(Test.class)).forEach(m -> {
+                try {
+                    m.invoke(cls);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+    }
 
-	private static class XUnitClassLoader extends ClassLoader {
-		@Override
-		protected Class<?> findClass(String name)
-				throws ClassNotFoundException {
-			try (var is = this.getResourceAsStream(name);) {
-				var array = IOUtils.toByteArray(is);
+    private static class XUnitClassLoader extends ClassLoader {
+        @Override
+        protected Class<?> findClass(String name)
+                throws ClassNotFoundException {
+            try (var is = this.getResourceAsStream(name);) {
+                var array = IOUtils.toByteArray(is);
 
-				return this.defineClass(this.convertClassName(name), array, 0,
-						array.length);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				throw new ClassNotFoundException();
-			}
-		}
+                return this.defineClass(XUnitClassLoader.convertClassName(name),
+                    array, 0,
+                    array.length);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new ClassNotFoundException();
+            }
+        }
 
-		private String convertClassName(String fileName) {
-			return fileName.substring(0, fileName.indexOf(".class"))
-					.replace("/", ".");
-		}
-	}
+        private static String convertClassName(String fileName) {
+            return fileName.substring(0, fileName.indexOf(".class"))
+                .replace("/", ".");
+        }
+    }
 }
