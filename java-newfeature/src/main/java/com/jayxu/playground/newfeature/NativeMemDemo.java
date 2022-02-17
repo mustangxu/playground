4b3c149686af394cdf33b16dbcefe9d2c67e2814
mustangxu <mustangxu@gmail.com>
2022-02-17 11:48:44 +0800

    upgrade libs

--- java-newfeature/src/main/java/com/jayxu/playground/newfeature/NativeMemDemo.java
+++ java-newfeature/src/main/java/com/jayxu/playground/newfeature/NativeMemDemo.java
@@ -4,10 +4,12 @@
 package com.jayxu.playground.newfeature;
 
 import jdk.incubator.foreign.MemorySegment;
+import jdk.incubator.foreign.ResourceScope;
 
 public class NativeMemDemo {
     public static void main(String[] args) {
-        var address = MemorySegment.allocateNative(4).address();
+        var address = MemorySegment
+            .allocateNative(4, ResourceScope.newConfinedScope()).address();
         System.out.println(address.toString());
     }
 }
