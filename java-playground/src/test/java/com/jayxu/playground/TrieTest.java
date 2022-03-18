3c11e0ad8c9cfa2e15d83120a12f6f7cde4bcc55
mustangxu <mustangxu@gmail.com>
2022-03-18 14:03:08 +0800

    add trie test

---
+++ java-playground/src/test/java/com/jayxu/playground/TrieTest.java
@@ -0,0 +1,31 @@
+package com.jayxu.playground;
+
+import org.apache.commons.collections4.map.HashedMap;
+import org.apache.commons.collections4.trie.PatriciaTrie;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.github.javafaker.Faker;
+
+// @RunWith(JQF.class)
+class TrieTest {
+    @Test
+    void test() {
+        var map = new HashedMap<String, String>();
+        var key = "ja";
+        var faker = new Faker();
+
+        for (var i = 0; i < 1000; i++) {
+            map.put(faker.name().username(), faker.name().fullName());
+        }
+        System.out.println(map.size());
+
+        Assertions.assertFalse(map.containsKey(key));
+
+        var trie = new PatriciaTrie<>(map);
+        Assertions.assertFalse(trie.containsKey(key));
+
+        var res = trie.prefixMap(key);
+        res.entrySet().stream().forEach(System.out::println);
+    }
+}
