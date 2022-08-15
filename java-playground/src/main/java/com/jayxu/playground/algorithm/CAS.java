0bcf2d7d68dea1a5d5822c775fc89aba79e2b601
mustangxu <mustangxu@gmail.com>
2022-08-15 15:28:53 +0800

    add CAS

---
+++ java-playground/src/main/java/com/jayxu/playground/algorithm/CAS.java
@@ -0,0 +1,34 @@
+/**
+ * Authored by jayxu @2022
+ */
+package com.jayxu.playground.algorithm;
+
+import lombok.Data;
+import sun.misc.Unsafe;
+
+/**
+ * @author xujiajing
+ */
+@Data
+public class CAS {
+    private static final Unsafe UNSAFE;
+    private static final long OFFSET;
+    private int value;
+
+    static {
+        try {
+            var field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            UNSAFE = (Unsafe) field.get(null);
+
+            OFFSET = UNSAFE
+                .objectFieldOffset(CAS.class.getDeclaredField("value"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean updateValue(int newValue) {
+        return UNSAFE.compareAndSwapInt(this, OFFSET, this.value, newValue);
+    }
+}
---
+++ java-playground/src/test/java/com/jayxu/playground/algorithm/CASTest.java
@@ -0,0 +1,127 @@
+package com.jayxu.playground.algorithm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+import org.eclipse.collections.impl.Counter;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import lombok.AllArgsConstructor;
+
+@BenchmarkMode(Mode.Throughput)
+@Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@Threads(2)
+@State(Scope.Benchmark)
+public class CASTest {
+    private ExecutorService executor;
+
+    public static void main(String[] args) throws Exception {
+        var opt = new OptionsBuilder()
+            .include(CASTest.class.getSimpleName()).build();
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        this.executor = Executors.newFixedThreadPool(8);
+    }
+
+    @TearDown
+    public void teardown() {
+        this.executor.shutdown();
+    }
+
+    @Test
+    void test() {
+        var cas = new CAS();
+        assertTrue(cas.updateValue(1));
+        assertEquals(1, cas.getValue());
+        assertTrue(cas.updateValue(1));
+
+        assertTrue(cas.updateValue(2));
+        assertEquals(2, cas.getValue());
+    }
+
+    @Benchmark
+    public void benchmarkCAS() throws Exception {
+        var cas = new CAS();
+
+        this.doBenchmark(() -> cas.updateValue(1));
+    }
+
+    @Benchmark
+    public void benchmarkSynchronized() throws Exception {
+        var c = new Counter();
+
+        this.doBenchmark(() -> {
+            synchronized (c) {
+                c.increment();
+            }
+
+            return c.getCount();
+        });
+    }
+
+    @Benchmark
+    public void benchmarkLock() throws Exception {
+        var l = new ReentrantLock();
+
+        this.doBenchmark(() -> {
+            var i = 0;
+            l.lock();
+            i++;
+            l.unlock();
+
+            return i;
+        });
+    }
+
+    @Benchmark
+    public void benchmarkAtomic() throws Exception {
+        var v = new AtomicInteger();
+
+        this.doBenchmark(() -> v.incrementAndGet());
+    }
+
+    private void doBenchmark(Supplier<?> sup) throws Exception {
+        var f1 = this.executor.submit(new LoopRunner(sup, 10));
+        var f2 = this.executor.submit(new LoopRunner(sup, 11));
+
+        f1.get();
+        f2.get();
+    }
+
+    @AllArgsConstructor
+    private static class LoopRunner<T> implements Callable<T> {
+        private Supplier<T> supplier;
+        private int times;
+
+        @Override
+        public T call() {
+            return this.supplier.get();
+        }
+    }
+}
