aaf95b23d050d0934f56f2abbb70e7bb6250847d
mustangxu <mustangxu@gmail.com>
2022-08-16 18:34:48 +0800

    add huffman tree

---
+++ java-playground/src/main/java/com/jayxu/playground/algorithm/HuffmanTree.java
@@ -0,0 +1,175 @@
+/**
+ * Authored by jayxu @2022
+ */
+package com.jayxu.playground.algorithm;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+
+import com.google.common.collect.Lists;
+import com.jayxu.playground.algorithm.HuffmanTree.HuffmanTreeNode;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author xujiajing
+ */
+public class HuffmanTree<T> extends Tree<T, HuffmanTreeNode<T>> {
+    private static final long serialVersionUID = -2503306000582479972L;
+    private LinkedList<HuffmanTreeNode<T>> dest;
+
+    @NoArgsConstructor
+    @Data
+    @ToString(callSuper = true)
+    @EqualsAndHashCode(callSuper = true)
+    public static class HuffmanTreeNode<T> extends TreeNode<T>
+            implements Comparable<HuffmanTreeNode<T>> {
+        private static final long serialVersionUID = 4547144338353998776L;
+        private int weight;
+
+        public HuffmanTreeNode(T value) {
+            super(value);
+        }
+
+        @Override
+        public int compareTo(HuffmanTreeNode<T> o) {
+            return Objects.compare(this, o,
+                Comparator.comparingInt(HuffmanTreeNode::getWeight));
+        }
+
+        @Override
+        protected HuffmanTreeNode<T> create(T v) {
+            return new HuffmanTreeNode<>(v);
+        }
+
+        @Override
+        protected boolean chooseLeft(T v) {
+            return true;
+        }
+    }
+
+    public HuffmanTree(Map<T, Integer> weightedMap) {
+        super(Order.LEVEL_ORDER);
+
+        this.dest = Lists.newLinkedList();
+        weightedMap.entrySet().stream().map(this::buildNode)
+            .sorted(Comparator.comparing(HuffmanTreeNode<T>::getWeight))
+            .forEach(this.dest::add);
+
+        this.buildTree();
+    }
+
+    private void buildTree() {
+        var copy = Lists.newLinkedList(this.dest);
+        this.size = copy.size();
+
+        while (copy.size() > 1) {
+            var n1 = copy.pollFirst();
+            var n2 = copy.pollFirst();
+
+            var p = this.buildParent(n1, n2);
+            copy.add(p);
+            Collections.sort(copy);
+        }
+
+        this.root = copy.pollFirst();
+        this.recalculateLevelsNDepth();
+    }
+
+    private HuffmanTreeNode<T> buildNode(Entry<T, Integer> entry) {
+        var node = new HuffmanTreeNode<T>();
+        node.value = entry.getKey();
+        node.weight = entry.getValue();
+
+        return node;
+    }
+
+    private HuffmanTreeNode<T> buildParent(HuffmanTreeNode<T> n1,
+            HuffmanTreeNode<T> n2) {
+        var node = new HuffmanTreeNode<T>();
+        node.weight = n1.weight + n2.weight;
+        n1.parent = node;
+        n2.parent = node;
+
+        if (n1.weight <= n2.weight) {
+            node.left = n1;
+            node.right = n2;
+        } else {
+            node.left = n2;
+            node.right = n1;
+        }
+
+        return node;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return TreeIterator.valueIterator(this.root, Order.LEVEL_ORDER);
+    }
+
+    @Override
+    public boolean add(T e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (this.dest.removeIf(n -> n.value != null && n.value.equals(o))) {
+            this.buildTree();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public byte[] encode(T v) {
+        if (this.root == null) {
+            return new byte[0];
+        }
+
+        var node = this.root.traverseMatch(v);
+        if (node == null) {
+            return new byte[0];
+        }
+
+        var code = new byte[node.level];
+        var p = node.parent;
+
+        if (node.isRoot()) {
+            code[0] = 0;
+        } else {
+            do {
+                code[node.level - 1] = (byte) (node == p.left ? 0 : 1);
+                node = p;
+                p = node.parent;
+            } while (p != null);
+        }
+
+        return code;
+    }
+
+    public T decode(byte[] code) {
+        TreeNode<T> node = this.root;
+
+        for (byte b : code) {
+            node = b == 0 ? node.left : node.right;
+        }
+
+        return node.value;
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        this.dest.clear();
+    }
+}
---
+++ java-playground/src/test/java/com/jayxu/playground/algorithm/HuffmanTreeTest.java
@@ -0,0 +1,67 @@
+/**
+ * Authored by jayxu @2022
+ */
+package com.jayxu.playground.algorithm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author xujiajing
+ */
+class HuffmanTreeTest {
+
+    @Test
+    void test() throws Exception {
+        Map<Character, Integer> freq = new ConcurrentHashMap<>();
+
+        try (var r = new BufferedReader(new InputStreamReader(
+            this.getClass().getClassLoader()
+                .getResourceAsStream("sample-text")));) {
+            r.lines().parallel().map(String::chars).reduce(IntStream::concat)
+                .get().parallel()
+                .forEach(ch -> freq.merge((char) ch, 1, Math::addExact));
+        }
+
+        var tree = new HuffmanTree<>(freq);
+
+        printTree(tree);
+
+        var code = tree.encode('e');
+        System.out.println(Arrays.toString(code));
+        assertEquals('e', tree.decode(code));
+
+        for (var ch = '0'; ch <= 'z'; ch++) {
+            System.out.println(ch + ": " + Arrays.toString(tree.encode(ch)));
+        }
+
+        var node = tree.root.traverseMatch('e');
+        System.out.println(node);
+        System.out.println(node.replace('!'));
+    }
+
+    @Test
+    void testRemove() {
+        var tree = new HuffmanTree<>(
+            Map.of('a', 1, 'e', 11, 'i', 3, 'o', 0, 'u', 7));
+
+        printTree(tree);
+        tree.remove('i');
+        printTree(tree);
+    }
+
+    private static void printTree(HuffmanTree<?> tree) {
+        tree.nodeStream().forEach(n -> System.out
+            .println(
+                n.getValue() + " [w: " + n.getWeight() + "], Lv."
+                    + n.getLevel()));
+    }
+}
