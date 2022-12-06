faeb567eabada7e6e03b7a858ae51bb554f5749a
Jay Xu <mustangxu@users.noreply.github.com>
2022-12-06 09:35:22 +0800

    disable experiment code temporarily

--- java-playground/src/main/java/com/jayxu/playground/experiment/NativeMemDemo.java
+++ java-playground/src/main/java/com/jayxu/playground/experiment/NativeMemDemo.java
@@ -1,17 +1,17 @@
-/**
- * Authored by jayxu @2021
- */
-package com.jayxu.playground.experiment;
-
-import jdk.incubator.foreign.MemorySegment;
-import jdk.incubator.foreign.ResourceScope;
-
-public class NativeMemDemo {
-    public static void main(String[] args) {
-        try (var res = ResourceScope.newConfinedScope();) {
-            var mem = MemorySegment
-                .allocateNative(1024, res);
-            System.out.println(mem.address().toString());
-        }
-    }
-}
+///**
+// * Authored by jayxu @2021
+// */
+//package com.jayxu.playground.experiment;
+//
+//import jdk.incubator.foreign.MemorySegment;
+//import jdk.incubator.foreign.ResourceScope;
+//
+//public class NativeMemDemo {
+//    public static void main(String[] args) {
+//        try (var res = ResourceScope.newConfinedScope();) {
+//            var mem = MemorySegment
+//                .allocateNative(1024, res);
+//            System.out.println(mem.address().toString());
+//        }
+//    }
+//}
