11f7ee920c359fd8985629cbece2f5078a8de2e3
mustangxu <mustangxu@gmail.com>
2022-12-26 18:33:36 +0800

    #refine code#

---
+++ java-playground/src/main/java/com/jayxu/playground/experiment/VectorDemo.java
@@ -0,0 +1,26 @@
+/// **
+// * Authored by jayxu @2022
+// */
+// package com.jayxu.playground.experiment;
+//
+// import jdk.incubator.vector.IntVector;
+// import jdk.incubator.vector.VectorSpecies;
+//
+/// **
+// * @author xujiajing
+// */
+// public class VectorDemo {
+// private static final VectorSpecies<Integer> SPECIES =
+/// IntVector.SPECIES_PREFERRED;
+//
+// public static void add(int[] aArray, int[] bArray) {
+// var va = IntVector.fromArray(SPECIES, aArray, 0);
+// var vb = IntVector.fromArray(SPECIES, bArray, 0);
+//
+// va.add(vb);
+// }
+//
+// public static void main(String[] args) {
+// VectorDemo.add(new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8 });
+// }
+// }
