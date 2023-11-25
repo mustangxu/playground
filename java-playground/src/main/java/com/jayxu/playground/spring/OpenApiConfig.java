23864f1aca79941965635e0fc3106e13996af796
Jay Xu <mustangxu@gmail.com>
2023-11-26 00:37:33 +0800

    add NPE tolerance

--- java-playground/src/main/java/com/jayxu/playground/spring/OpenApiConfig.java
+++ java-playground/src/main/java/com/jayxu/playground/spring/OpenApiConfig.java
@@ -3,6 +3,7 @@
  */
 package com.jayxu.playground.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.info.GitProperties;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +17,19 @@ import io.swagger.v3.oas.models.info.Info;
  */
 @Configuration
 public class OpenApiConfig {
+    @Autowired(required = false)
+    private BuildProperties build;
+    @Autowired(required = false)
+    private GitProperties git;
+
     @Bean
-    OpenAPI springShopOpenAPI(BuildProperties build, GitProperties git) {
-        return new OpenAPI().info(new Info().title("Jay's Coding Playground")
-            .version(String.format("v%s-%s @%s", build.getVersion(),
-                git.getShortCommitId(), git.getCommitTime())));
+    OpenAPI springShopOpenAPI() {
+        var info = new Info().title("Jay's Coding Playground");
+        if (this.build != null && this.git != null) {
+            info.version(String.format("v%s-%s @%s", this.build.getVersion(),
+                this.git.getShortCommitId(), this.git.getCommitTime()));
+        }
+
+        return new OpenAPI().info(info);
     }
 }
