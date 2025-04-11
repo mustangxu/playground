b93a663992dd48cd693f3fda817dbf63bae3bb0b
Jay Xu <mustangxu@gmail.com>
2025-04-12 00:42:14 +0800

    move demo code

--- mcp/src/main/java/com/jayxu/playground/spring/ai/McpApplication.java
+++ mcp/src/main/java/com/jayxu/playground/spring/ai/McpApplication.java
@@ -3,22 +3,14 @@
  */
 package com.jayxu.playground.spring.ai;
 
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.reactive.function.client.WebClient;
-import io.modelcontextprotocol.client.McpClient;
-import io.modelcontextprotocol.client.transport.WebFluxSseClientTransport;
-import io.modelcontextprotocol.spec.McpSchema;
-
-import lombok.extern.slf4j.Slf4j;
+import com.jayxu.playground.spring.ai.mcp.ClientDemo;
 
 /**
  * @author jayxu
  */
-@Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = "com.jayxu")
 public class McpApplication {
@@ -26,25 +18,6 @@ public class McpApplication {
     public static void main(String[] args) {
         SpringApplication.run(McpApplication.class, args);
 
-        Thread.ofVirtual().start(() -> {
-            try (var client = McpClient.sync(
-                    new WebFluxSseClientTransport(WebClient.builder().baseUrl("http://localhost:8080"))).build()) {
-
-                client.initialize();
-                client.ping();
-
-                // List and demonstrate tools
-                client.listTools().tools().forEach(t -> log.info("Tools: {}", t));
-
-                var weatherForcastResult = client.callTool(new McpSchema.CallToolRequest("getWeatherForecastByLocation",
-                                                                                         Map.of("latitude", "47.6062",
-                                                                                                "longitude",
-                                                                                                "-122.3321")));
-                log.info("Weather Forcast: {}", weatherForcastResult);
-
-                var alertResult = client.callTool(new McpSchema.CallToolRequest("getAlerts", Map.of("state", "NY")));
-                log.info("Alert Response: {}", alertResult);
-            }
-        });
+        Thread.ofVirtual().start(() -> new ClientDemo().start());
     }
 }
---
+++ mcp/src/main/java/com/jayxu/playground/spring/ai/mcp/ClientDemo.java
@@ -0,0 +1,38 @@
+/*
+ * Authored by jayxu @2025
+ */
+
+package com.jayxu.playground.spring.ai.mcp;
+
+import java.util.Map;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.transport.WebFluxSseClientTransport;
+import io.modelcontextprotocol.spec.McpSchema;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ClientDemo {
+    public static void main(String[] args) {
+        new ClientDemo().start();
+    }
+
+    public void start() {
+        try (var client = McpClient.sync(
+                new WebFluxSseClientTransport(WebClient.builder().baseUrl("http://127.0.0.1:8080"))).build()) {
+
+            client.initialize();
+            client.ping();
+
+            client.listTools().tools().forEach(t -> log.info("{}", t));
+
+            log.info("Weather Forcast: {}", client.callTool(
+                    new McpSchema.CallToolRequest("getWeatherForecastByLocation",
+                                                  Map.of("latitude", "47.6062", "longitude", "-122.3321"))));
+
+            log.info("Alerts: {}", client.callTool(new McpSchema.CallToolRequest("getAlerts", Map.of("state", "NY"))));
+        }
+    }
+}
