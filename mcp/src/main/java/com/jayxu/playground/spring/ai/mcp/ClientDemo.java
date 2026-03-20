5f77b8356fbb45a680e7d260ae2c2a84567d0ec8
Jay Xu <mustangxu@gmail.com>
2026-03-20 16:01:51 +0800

    fix compile error

--- mcp/src/main/java/com/jayxu/playground/spring/ai/mcp/ClientDemo.java
+++ mcp/src/main/java/com/jayxu/playground/spring/ai/mcp/ClientDemo.java
@@ -6,14 +6,13 @@ package com.jayxu.playground.spring.ai.mcp;
 
 import java.util.Map;
 
+import org.springframework.ai.mcp.client.webflux.transport.WebFluxSseClientTransport;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.modelcontextprotocol.client.McpClient;
-import io.modelcontextprotocol.client.transport.WebFluxSseClientTransport;
-import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
+import io.modelcontextprotocol.json.jackson3.JacksonMcpJsonMapper;
 import io.modelcontextprotocol.spec.McpSchema;
+import tools.jackson.databind.json.JsonMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,7 +25,7 @@ public class ClientDemo {
     public void start() {
         try (var client = McpClient.sync(
                 new WebFluxSseClientTransport(WebClient.builder().baseUrl("http://127.0.0.1:8080"),
-                        new JacksonMcpJsonMapper(new ObjectMapper()))).build()) {
+                        new JacksonMcpJsonMapper(new JsonMapper()))).build()) {
 
             client.initialize();
             client.ping();
